58cd0f6838b38ea1560bc9adbba70439ae2b2a2a
Barry Richards <barry@br-web.me>
2026-01-07 00:58:05 +0000

    Theme and content import

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,8 +1,6 @@
 import {BackTop, BackTopVisibility} from './BackTop.js';
-import {InitBlinkWidget} from './BlinkInit.js';
 import {AddLoaded} from './AddLoaded.js';
 
 BackTop();
 BackTopVisibility();
-InitBlinkWidget();
 AddLoaded();
